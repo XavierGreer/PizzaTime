31eee782bae0569a3771d2114fd3e5104a89941c
Xavier Greer <work.xavier.greer@gmail.com>
2021-04-07 22:45:50 -0600

    Database is remade, users will need to be created again. Admin page now has additional funcitonalities for adding new items such as toppings, pizzas, sides, sodas, etc.

--- web_pizza/admin.py
+++ web_pizza/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product, Topping
+from .models import Category, Product, Topping, Pizza, ToppingAmount, Soda, Sides, Order, Customer
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -13,9 +13,33 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ['price','available']
     prepopulated_fields = {'slug': ('name',)}
 
+class ToppingAmountInline(admin.TabularInline):
+    model = ToppingAmount
+
 @admin.register(Topping)
 class ToppingAdmin(admin.ModelAdmin):
     list_display = ['name']
+
+@admin.register(Pizza)
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ['name','size','price']
+    inlines = [ToppingAmountInline, ]
+
+@admin.register(Soda)
+class SodaAdmin(admin.ModelAdmin):
+    list_display = ['name','size','price']
+
+@admin.register(Sides)
+class SidesAdmin(admin.ModelAdmin):
+    list_display = ['name','price']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['orderID', 'customer', 'status', 'total', 'discount',]
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['customerID', 'firstname', 'lastname', 'phone', 'email', 'address', 'zipcode']
 
 '''
 Admin test account information.
--- web_pizza/models.py
+++ web_pizza/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+import uuid
+from django.core.validators import RegexValidator
 from django.contrib.auth import get_user_model
 
 class Category(models.Model):
@@ -17,51 +19,42 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('product_list:product_list_by_category',args=[self.slug])
 
-
-class Topping(models.Model):
-    name = models.CharField(max_length=20, db_index=True)
-
-    class Meta:
-        ordering = ('name',)
-        verbose_name = 'topping'
-        verbose_name_plural = 'toppings'
-
-    def __str__(self):
-        return self.name
-
-'''
 class Pizza(models.Model):
-    price_small = models.DecimalField(max_digits=8, decimal_places=2)
-    price_big = models.DecimalField(max_digits=8, decimal_places=2)
     name = models.CharField(max_length=60)
-    topping = models.ForeignKey(Topping, on_delete=models.CASCADE, blank=True)
+    topping = models.ManyToManyField('Topping', through='ToppingAmount')
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     PIZZA_SIZES = (
-        (12, 'Medium'),
+        (12, 'Small'),
+        (14, 'Medium'),
         (16, 'Large'),
     )
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    size = models.IntegerField(choices=PIZZA_SIZES, default=12, blank=False)
+
+    class Meta:
+        ordering = ('name','size')
+        verbose_name = 'pizza'
+        verbose_name_plural = 'pizzas'
 
     def __str__(self):
         return self.name
 
-class Size(models.Model):
-    name = models.CharField(max_length=10)
+class ToppingAmount(models.Model):
+    Nothing = 0
+    Regular = 1
+    Double = 2
+    CHOICES = (
+        (Nothing, 'Nothing'),
+        (Regular, 'Regular'),
+        (Double, 'Double'),
+    )
 
-
-class Order(models.Model):
-    pizza = models.ForeignKey('main.Pizza', on_delete=models.PROTECT)
-    size = models.IntegerField(choices=Pizza.PIZZA_SIZES, default=30, blank=False)
-    customer_name = models.CharField(max_length=100)
-    customer_address = models.TextField(null=False, blank=False)
-
-    @property
-    def price(self):
-        """ Returns the price of the order. """
-        if self.size == 30:
-            return self.pizza.price_small
-        if self.size == 50:
-            return self.pizza.price_big
-        return self.pizza.price_small
-'''
+    pizza = models.ForeignKey('Pizza', on_delete=models.CASCADE, null=True, blank=True)
+    topping = models.ForeignKey('Topping', on_delete=models.CASCADE, null=True, blank=True)
+    amount = models.IntegerField(choices=CHOICES, default=Nothing)
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
@@ -84,3 +77,76 @@ class Product(models.Model):
     def get_absolute_url(self):
         return reverse('product_list:product_detail',args=[self.id,self.slug])
 
+class Topping(models.Model):
+    name = models.CharField(max_length=20, db_index=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'topping'
+        verbose_name_plural = 'toppings'
+
+    def __str__(self):
+        return self.name
+
+class Soda(models.Model):
+    name = models.CharField(max_length=20, db_index=True)
+    SODA_SIZES = (
+        (1, '12 Oz'),
+        (2, '2 Liter'),
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.IntegerField(choices=SODA_SIZES, default=12, blank=False)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name', 'size', 'price')
+        verbose_name = 'soda'
+        verbose_name_plural = 'sodas'
+
+    def __str__(self):
+        return self.name
+
+class Sides(models.Model):
+    name = models.CharField(max_length=20, db_index=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name', 'price',)
+        verbose_name = 'side'
+        verbose_name_plural = 'sides'
+
+    def __str__(self):
+        return self.name
+
+class Customer(models.Model):
+    customerID = models.CharField(primary_key=True, default=uuid.uuid4().hex[:5].upper(), max_length=100, editable=False)
+    firstname = models.CharField(max_length=50, db_index=True)
+    lastname = models.CharField(max_length=50, db_index=True)
+    phone = models.CharField(max_length=10, null=True, validators=[RegexValidator(r'^\d{3}-\d{3}-\d{4}$')])
+    email = models.EmailField(max_length = 254)
+    address = models.CharField("Address line 1", max_length=1024,)
+    zipcode = models.CharField("ZIP / Postal code", max_length=12, editable=False)
+
+class Order(models.Model):
+    orderID = models.CharField(primary_key=True, default=uuid.uuid4().hex[:5].upper(), max_length=100, editable=False)
+    customer = models.ForeignKey('Customer', on_delete=models.CASCADE, null=True, blank=True, editable=False)
+    total = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
+    tax = models.DecimalField(max_digits=4, decimal_places=2, editable=False)
+    discount = models.DecimalField(max_digits=2, decimal_places=0,)
+    STATUSES = (
+        (0, 'Order Received'),
+        (1, 'Baking'),
+        (2, 'On The Way'),
+        (3, 'Delivered'),
+    )
+    status = models.IntegerField(choices=STATUSES, default=0, blank=False)
+
+    def __str__(self):
+        return str(self.orderID)
