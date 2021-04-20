63c561e258917890d158762f1720da407e440e2b
j.hill291 <j.hill291@my.cs.coloradotech.edu>
2021-04-19 19:14:43 -0600

    Contact Us Page added in separate folder. About Us Page updated and tried to hyper link to base.html. Jonathan Hill

--- pizza_time/urls.py
+++ pizza_time/urls.py
@@ -18,10 +18,11 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cart/',include('cart.urls')),
-    path('', include('web_pizza.urls',namespace='webpizza')),
+    path('cart/', include('cart.urls')),
+    path('', include('web_pizza.urls', namespace='web_pizza')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
